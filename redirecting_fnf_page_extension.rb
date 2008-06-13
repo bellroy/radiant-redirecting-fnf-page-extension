702686a3c1a9ffc86aa44c0bbd16289955a1c7cb
Glenn Murray <glenn@trike.com.au>
2008-06-13 15:00:01 +1000

    Windows -a didn't add missing file. Adding

---
+++ redirecting_fnf_page_extension.rb
@@ -0,0 +1,13 @@
+# Uncomment this if you reference any of your controllers in activate
+# require_dependency 'application'
+
+class RedirectingFnfPageExtension < Radiant::Extension
+  version "1.0"
+  description "FileNotFound page type that can serve 301 or 302 redirects for moved pages."
+  url "http://github.com/tricycle/radiant-redirecting-fnf-page-extension/"
+  
+  def activate
+    FileNotFoundExtPage
+  end
+  
+end
